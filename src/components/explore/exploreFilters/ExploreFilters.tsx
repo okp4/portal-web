5b40bb66c874b6a1b8424603c8dd2a24ee5e6ef1
RaphaelEscrig <raphael.escrig@hotmail.fr>
2022-09-22 15:08:37 +0200

    feat: add explore layout with fake data
    Signed-off-by: RaphaelEscrig <raphael.escrig@hotmail.fr>

---
+++ src/components/explore/exploreFilters/ExploreFilters.tsx
@@ -0,0 +1,5 @@
+const ExploreFilters = (): JSX.Element => {
+  return <div className="okp4-explore-filters"></div>
+}
+
+export default ExploreFilters;
---
+++ src/components/explore/exploreListConfiguration/constants/CExploreListConfigurationSortBy.constant.ts
@@ -0,0 +1,8 @@
+import type { SelectOption } from "@okp4/ui";
+
+export const CExploreListConfigurationSortBy: Array<SelectOption> = [
+  {
+    label: "Alphabetical",
+    value: "asc",
+  },
+]

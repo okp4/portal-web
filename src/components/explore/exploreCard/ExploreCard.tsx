09514745746e6a78f72fec107e28892ed1c4519c
RaphaelEscrig <raphael.escrig@hotmail.fr>
2022-09-27 17:48:54 +0200

    feat: add translations

--- src/components/explore/exploreCard/ExploreCard.tsx
+++ src/components/explore/exploreCard/ExploreCard.tsx
@@ -1,35 +1,44 @@
-import { Button, Typography } from '@okp4/ui'
+import { Button, Typography, useTranslation } from '@okp4/ui'
+import type { UseTranslationResponse } from '@okp4/ui'
 import { format } from 'date-fns'
 import { PortalCard } from '../../portalCard/PortalCard'
 import './ExploreCard.scss'
 
-export const ExploreCard = (): JSX.Element => (
-  <div className="okp4-explore-card">
-    <PortalCard>
-      <>
-        <Typography as="h2" color="inverted-text" fontSize="large" fontWeight="bold">
-          Agreste
-        </Typography>
+export const ExploreCard = (): JSX.Element => {
+  const { t }: UseTranslationResponse = useTranslation()
 
-        <Typography as="span" color="inverted-text" fontSize="medium">
-          Dataset
-        </Typography>
-        <Typography as="span" color="inverted-text" fontSize="small">
-          Public
-        </Typography>
-        <Typography as="span" color="inverted-text" fontSize="small">
-          Agriculture, Open data, Census, Dataviz, Agreste
-        </Typography>
-        <Typography
-          as="span"
-          color="inverted-text"
-          fontSize="x-small"
-        >{`By Agreste, Last update ${format(new Date(), 'dd/MM/yyyy')}`}</Typography>
+  return (
+    <div className="okp4-explore-card">
+      <PortalCard>
+        <>
+          <Typography as="h2" color="inverted-text" fontSize="large" fontWeight="bold">
+            Agreste
+          </Typography>
 
-        <Button backgroundColor="secondary" label="Add this Dataset to my Dataspace" size="large" />
-      </>
-    </PortalCard>
-  </div>
-)
+          <Typography as="span" color="inverted-text" fontSize="medium">
+            Dataset
+          </Typography>
+          <Typography as="span" color="inverted-text" fontSize="small">
+            Public
+          </Typography>
+          <Typography as="span" color="inverted-text" fontSize="small">
+            Agriculture, Open data, Census, Dataviz, Agreste
+          </Typography>
+          <Typography as="span" color="inverted-text" fontSize="x-small">{`${t(
+            'explore:by'
+          )} Agreste, ${t('explore:updatedAt')} ${format(new Date(), 'dd/MM/yyyy')}`}</Typography>
+
+          <Button
+            backgroundColor="secondary"
+            label={`${t('explore:addThis')} ${t(`explore:${'dataset'}`)} ${t(
+              'explore:toDataspace'
+            )}`}
+            size="large"
+          />
+        </>
+      </PortalCard>
+    </div>
+  )
+}
 
 export default ExploreCard
--- src/components/goBack/Goback.tsx
+++ src/components/goBack/Goback.tsx
@@ -1,18 +1,20 @@
-import { Button, Icon } from '@okp4/ui'
+import { Button, Icon, useTranslation } from '@okp4/ui'
+import type { UseTranslationResponse } from '@okp4/ui'
 import { useRouter } from 'next/router'
 import type { NextRouter } from 'next/router'
 import { useCallback } from 'react'
 
 export const GoBack = (): JSX.Element => {
   const router: NextRouter = useRouter()
+  const { t }: UseTranslationResponse = useTranslation()
 
   const handleClick = useCallback((): void => {
     router.back()
   }, [router])
 
   return (
-    <div className='okp4-go-back'>
-      <Button label="Back" leftIcon={<Icon name="arrow-left" size={22} />} onClick={handleClick} />
+    <div className="okp4-go-back">
+      <Button label={t("explore:back")} leftIcon={<Icon name="arrow-left" size={22} />} onClick={handleClick} />
     </div>
   )
 }

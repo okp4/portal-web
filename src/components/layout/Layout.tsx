bf9c01f3fcbcb9f37e48aff5b7124fc52cd2a71b
lauren olivier <laurenzolivier@gmail.com>
2022-10-07 17:39:15 +0200

    feat: add navigation menu with router handling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,29 +1,193 @@
-import type { DeepReadonly } from '@okp4/ui'
-import React from 'react'
+import { Footer, Header, Logo, Typography, useTheme, useTranslation } from '@okp4/ui'
+import type {
+  DeepReadonly,
+  NavigationItem,
+  ThemeContextType,
+  UseTranslationResponse
+} from '@okp4/ui'
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
+import lightCosmos from '@okp4/ui/lib/assets/images/cosmos-clear.png'
+import darkCosmos from '@okp4/ui/lib/assets/images/cosmos-dark.png'
+import '../../i18n/index'
+import './layout.scss'
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 type LayoutProps = {
   readonly children: React.ReactNode
 }
 
-// Components using document or window elements must disable ssr to be used on client side
-// https://nextjs.org/docs/advanced-features/dynamic-import#with-no-ssr
-const ContentWithoutSSR = dynamic(async () => import('../content/Content'), {
-  ssr: false
-})
-export const Layout = ({ children }: DeepReadonly<LayoutProps>): JSX.Element => (
-  <>
-    <Head>
-      <title>OKP4 Portal</title>
-      <meta
-        content="OKP4, Portal, Data App, Dataverse, Services, Dataset, Data Space, Deposit, Catalog, Files, Blockchain, Metadata, Exploration, Know, Token"
-        name="keywords"
-      />
-      <link href="/okp4-logo.png" rel="icon" type="image/x-icon" />
-    </Head>
-    <main>
-      <ContentWithoutSSR>{children}</ContentWithoutSSR>
-    </main>
-  </>
-)
+type FooterLinkProps = {
+  readonly label: string
+}
+
+const languages = [
+  {
+    name: 'English',
+    lng: 'en'
+  },
+  {
+    name: 'Français',
+    lng: 'fr'
+  }
+]
+
+const Okp4Link = ({ label }: FooterLinkProps): JSX.Element => {
+  return (
+    <Typography as="p" color="invariant-text" fontSize="x-small" fontWeight="xlight" noWrap>
+      {`${label} `}
+      <Typography color="invariant-text" fontSize="x-small" fontWeight="bold">
+        <a
+          className="okp4-brand-link"
+          href="https://okp4.network/"
+          rel="author noreferrer"
+          target="_blank"
+        >
+          ØKP4
+        </a>
+      </Typography>
+    </Typography>
+  )
+}
+
+// eslint-disable-next-line max-lines-per-function
+const Layout = ({ children }: DeepReadonly<LayoutProps>): JSX.Element => {
+  const { theme }: ThemeContextType = useTheme()
+  const { t }: UseTranslationResponse = useTranslation()
+  const themedImage = theme === 'light' ? lightCosmos.src : darkCosmos.src
+  const footerLabel = t('footer:brand-link')
+  const router = useRouter()
+  console.log(router)
+
+  const PhilosophyNavItem = (
+    <Typography as="div" fontSize="small" fontWeight="light">
+      <a
+        href="https://ui.okp4.space/?path=/docs/philosophy--page"
+        rel="noreferrer"
+        style={{ wordBreak: 'normal' }}
+        target="_blank"
+      >
+        Philosophy
+      </a>
+    </Typography>
+  )
+  const DesignSystemNavItem = (
+    <Typography as="div" fontSize="small" fontWeight="light">
+      <a
+        href="https://ui.okp4.space/?path=/docs/welcome--page"
+        rel="noreferrer"
+        style={{ wordBreak: 'normal' }}
+        target="_blank"
+      >
+        The OKP4 Design System
+      </a>
+    </Typography>
+  )
+  const StartNavItem = (
+    <Typography as="div" fontSize="small" fontWeight="light">
+      <a
+        href="https://ui.okp4.space/?path=/docs/getting-started--page"
+        rel="noreferrer"
+        style={{ wordBreak: 'normal' }}
+        target="_blank"
+      >
+        Launch project
+      </a>
+    </Typography>
+  )
+  const CreateNavItem: NavigationItem = {
+    isSelectedFromStart: router.pathname === '/',
+    menuItem: (
+      <Typography as="div" fontSize="small" fontWeight="bold">
+        <a
+          href="https://xd.adobe.com/view/0994d8fe-0e3f-44e0-b8bf-3c399bf524d9-5cc7/screen/007b4c59-2299-452d-90d6-6218e46b55f9/"
+          rel="noopener noreferrer"
+          style={{ wordBreak: 'normal' }}
+          target="_blank"
+        >
+          {t('header:create')}
+        </a>
+      </Typography>
+    )
+  }
+
+  const ExploreNavItem: NavigationItem = {
+    isSelectedFromStart: router.pathname === '/explore',
+    menuItem: (
+      <Typography as="div" fontSize="small" fontWeight="bold">
+        <Link href="/explore">{t('header:explore')}</Link>
+      </Typography>
+    )
+  }
+  const InteractNavItem: NavigationItem = {
+    menuItem: (
+      <Typography as="div" fontSize="small" fontWeight="bold">
+        {t('header:interact')}
+      </Typography>
+    ),
+    subMenu: [DesignSystemNavItem, PhilosophyNavItem, StartNavItem]
+  }
+
+  const LearnNavItem: NavigationItem = {
+    menuItem: (
+      <Typography as="div" fontSize="small" fontWeight="bold">
+        <a
+          href="https://docs.okp4.network/docs/whitepaper/abstract"
+          rel="noopener noreferrer"
+          style={{ wordBreak: 'normal' }}
+          target="_blank"
+        >
+          {t('header:learn')}
+        </a>
+      </Typography>
+    )
+  }
+
+  const okp4NavItem: NavigationItem = {
+    menuItem: (
+      <Typography as="div" fontSize="small" fontWeight="bold">
+        <a
+          href="https://okp4.network/#protocol"
+          rel="noopener noreferrer"
+          style={{ wordBreak: 'normal' }}
+          target="_blank"
+        >
+          {t('header:okp4')}
+        </a>
+      </Typography>
+    )
+  }
+
+  const navMenu = [CreateNavItem, ExploreNavItem, InteractNavItem, LearnNavItem, okp4NavItem]
+  const backHome = (): void => {
+    router.push('/')
+  }
+
+  return (
+    <>
+      <Head>
+        <title>OKP4 Portal</title>
+        <meta
+          content="OKP4, Portal, Data App, Dataverse, Services, Dataset, Data Space, Deposit, Catalog, Files, Blockchain, Metadata, Exploration, Know, Token"
+          name="keywords"
+        />
+        <link href="/okp4-logo.png" rel="icon" type="image/x-icon" />
+      </Head>
+      <div id="layout">
+        <Header
+          firstElement={
+            <div onClick={backHome} style={{ cursor: 'pointer' }}>
+              <Logo size="small" />
+            </div>
+          }
+          navigationMenu={navMenu}
+        />
+        <main style={{ backgroundImage: `url(${themedImage})` }}>{children}</main>
+        <Footer languages={languages} lastElement={<Okp4Link label={footerLabel} />} />
+      </div>
+    </>
+  )
+}
+
+export default Layout

092753d5a3a6490a2659f7db988ac3bc7532a168
lauren olivier <laurenzolivier@gmail.com>
2022-10-18 16:05:58 +0200

    chore: add api config with server url env variable

---
+++ src/pages/api/config.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export type Config = {
+  app: {
+    governanceUrl: string
+  }
+}
+
+export const server = process.env.SERVER_URL
+
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+export default function handler(_req: NextApiRequest, res: NextApiResponse<Config>): void {
+  const config: Config = {
+    app: {
+      governanceUrl: process.env.GOVERNANCE_URL
+    }
+  }
+  return res.status(200).json(config)
+}

35fa377a75a4a825c00e77b1c1bdf87f89b6a143
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-31 21:27:34 +0100

    chore: add window environment file

---
+++ window.d.ts
@@ -0,0 +1,5 @@
+import type { Window as KeplrWindow } from '@keplr-wallet/types'
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Window extends KeplrWindow {}
+}

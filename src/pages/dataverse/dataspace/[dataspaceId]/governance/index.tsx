0f60da03d48263c620c39e1dabbad3f5fca7f57c
ErikssonJoakim <j.eriksson@okp4.com>
2022-12-14 15:54:40 +0100

    feat: add governance page

---
+++ src/pages/dataverse/dataspace/[dataspaceId]/governance/index.tsx
@@ -0,0 +1,45 @@
+import type { DeepReadonly } from '@okp4/ui'
+import type {
+  GetStaticPaths,
+  GetStaticPathsResult,
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
+  NextPage
+} from 'next'
+import type { DataspaceDto } from '../../../../../dto/DataspaceDto'
+
+type GovernanceProps = {
+  dataspace: DataspaceDto | null
+}
+
+const Governance: NextPage<GovernanceProps> = () => {
+  return <div className="okp4-governance-main"></div>
+}
+
+export default Governance
+
+export const getStaticPaths: GetStaticPaths = async (): Promise<GetStaticPathsResult> => {
+  return {
+    paths: [],
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params
+}: DeepReadonly<GetStaticPropsContext>): Promise<
+  GetStaticPropsResult<Omit<GovernanceProps, 'config'>>
+> => {
+  const dataspaceId = params?.dataspaceId
+
+  const dataspaceResponse = await fetch(`${process.env.API_URI}/dataverse/dataspace/${dataspaceId}`)
+
+  const dataspace: DataspaceDto | null = dataspaceResponse.ok
+    ? await dataspaceResponse.json()
+    : null
+
+  return {
+    props: { dataspace }
+  }
+}
